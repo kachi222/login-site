d4e994da0ba28cc9b2e3bb908ef78a8c798c94a1
kachi222 <enderleenkriss@gmail.com>
2022-02-16 12:54:55 -0800

    editing

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
  const hostname = '127.0.0.1'
 const app = express();
+const path = require ('path')
 const dotenv = require('dotenv')
 const routes = require("./routes/index");
 const connectDB = require('./config/db')
 dotenv.config('.env')
 connectDB();
+
+app.use('/css', express.static(path.join(__dirname,'public/css')))
+
+app.use('/img', express.static(path.join(__dirname,'public/img')))
+
+app.set('view engine', 'ejs')
 
 // parse request of content typle - application/json
 app.use(express.json());
@@ -21,17 +28,21 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/registration', (req, res) =>{
-    res.render('registration');
-})
-
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-
-app.use('/img', express.static(path.join(__dirname,'public/img')))
-
-app.set('view engine', 'ejs')
+app.get('/home', (req, res) => {
+    res.render('home')
+  })
+  
+  app.get('/registration', (req, res) => {
+     res.render('registration');
+  })
+  
+  app.get('/login', (req, res) => {
+     res.render('login');
+  })
+  
+  app.get('/secretPage', (req, res) => {
+    res.render('secretPage');
+  })
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, hostname, ()=>{
